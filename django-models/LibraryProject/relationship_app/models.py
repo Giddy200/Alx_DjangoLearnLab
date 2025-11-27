c8d1726d1a5846d5114e9cbd5d224f32b0fd918d
Opeyemi Ogunfeyimi <gideongifty200@gmail.com>
2025-11-27 22:22:39 +0100

    Puuting models in place

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -5,17 +5,18 @@ class Author(models.Model):
     def __str__(self):
         return self.name
 
-class Book (models.Model):
+
+class Book(models.Model):
     title = models.CharField (max_length=200)
-    Author = models.ForeignKey (Authour, on_delete=models.CASCADE)
+    author = models.ForeignKey (Author, on_delete=models.CASCADE)
     publication_year = models.IntegerField()
 
-class Libary (models.Model):
+class Libary(models.Model):
         name = models.CharField (max_length=100)
         location = models.CharField (max_length=200)
         books = models.ManyToManyField (Book)
 
-class Libarian (models.Model):
+class Libarian(models.Model):
     name = models.CharField (max_length=100)
     library = models.OneToOneField (Libary, on_delete=models.CASCADE)        
 
