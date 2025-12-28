a21ee1735e15fe3a9f77491485e5dc2b9e2628a1
Opeyemi Ogunfeyimi <gideongifty200@gmail.com>
2025-12-28 09:21:22 +0100

    I added initial settings for installed Django apps

---
+++ Dawn/dawnin_pro.settings.py
@@ -0,0 +1,11 @@
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'rest_framework',  # Example of a custom app
+    'django_filters',  # Another custom app
+    'tasks',  # Your tasks app
+]
